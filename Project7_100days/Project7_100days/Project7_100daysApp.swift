7275fec014fb7735f3abd334a05ae3d8f492a9a1
Aleksandr Zubov <alexanderzubov@icloud.com>
2023-01-11 00:50:57 +0100

    initial_commit_dev

---
+++ Project7_100days/Project7_100days/Project7_100daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project7_100daysApp.swift
+//  Project7_100days
+//
+//  Created by user228564 on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Project7_100daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project7_100days/Project7_100daysUITests/Project7_100daysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project7_100daysUITestsLaunchTests.swift
+//  Project7_100daysUITests
+//
+//  Created by user228564 on 1/11/23.
+//
+
+import XCTest
+
+final class Project7_100daysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
